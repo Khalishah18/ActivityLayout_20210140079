2ac65cf23167371e34daa2e4d354387ddd105dbe
Khalishah18 <khalishahsalsabilla18@gmail.com>
2023-10-05 15:58:50 +0700

    fix error

---
+++ app/src/main/java/com/example/activity1/FrameLayoutActivity.kt
@@ -0,0 +1,11 @@
+package com.example.activity1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FrameLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linear_layout)
+    }
+}
